96cdbdfb370de0a8a985404cb38e1b67093ece7f
Zain Ul Abaiden <76576809+Zain-Ul-Abaiden@users.noreply.github.com>
2023-08-17 17:41:13 +0500

    Second commit

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,25 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="flex min-h-full">
+    <div class="flex w-full flex-col">
+      <Navbar />
+      <HeroComponent />
+      <Footbar />
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import Navbar from '@/components/Nav.vue'
+import Footbar from '@/components/Foot.vue'
+import HeroComponent from '@/components/Hero.vue'
 
 export default {
-  name: 'HomeView',
+  name: "HomeView",
+
   components: {
-    HelloWorld
-  }
-}
+    Navbar,
+    Footbar,
+    HeroComponent,
+  },
+};
 </script>
